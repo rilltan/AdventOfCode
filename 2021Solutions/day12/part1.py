36a603d96888257fca8f2b8e23ee1463e16e57f4
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-11-30 18:07:14 +0000

    Did 2021 day 12

--- 2021Solutions/day12/part1.py
+++ 2021Solutions/day12/part1.py
@@ -4,3 +4,30 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+graph = {}
+for a,b in [x.split("-") for x in data]:
+    if a not in graph:
+        graph[a] = list()
+    graph[a].append(b)
+
+    if b not in graph:
+        graph[b] = list()
+    graph[b].append(a)
+
+def dfs(graph, visited = ["start"]):
+    paths = 0
+    if visited[-1] == "end":
+        return 1
+    else:
+        for x in graph[visited[-1]]:
+            if x.isupper():
+                new = [y for y in visited]
+                new.append(x)
+                paths += dfs(graph,new)
+            elif x not in visited and x != "start":
+                new = [y for y in visited]
+                new.append(x)
+                paths += dfs(graph,new)
+        return paths
+
+prco(dfs(graph))
--- 2021Solutions/day12/part2.py
+++ 2021Solutions/day12/part2.py
@@ -4,3 +4,35 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+graph = {}
+for a,b in [x.split("-") for x in data]:
+    if a not in graph:
+        graph[a] = list()
+    graph[a].append(b)
+
+    if b not in graph:
+        graph[b] = list()
+    graph[b].append(a)
+
+def dfs(graph, visited = ["start"], visitedTwo = False):
+    paths = 0
+    if visited[-1] == "end":
+        return 1
+    else:
+        for x in graph[visited[-1]]:
+            if x.isupper():
+                visited.append(x)
+                paths += dfs(graph,visited,visitedTwo)
+                visited.pop()
+            elif x != "start":
+                if x not in visited:
+                    visited.append(x)
+                    paths += dfs(graph,visited,visitedTwo)
+                    visited.pop()
+                elif not visitedTwo:
+                    visited.append(x)
+                    paths += dfs(graph,visited,True)
+                    visited.pop()
+        return paths
+
+prco(dfs(graph))
