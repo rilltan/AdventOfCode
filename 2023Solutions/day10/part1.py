64e44a896b50aaf554bf59aff821d3b2049a9160
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-11-30 18:24:05 +0000

    Delete 2023 solutions

--- 2023Solutions/day10/part1.py
+++
@@ -1,6 +0,0 @@
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-from utilities import *
-data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
-
