f08ece519ebbe19f69619e99c91d3cb1c1a70ddd
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-01 09:15:07 +0000

    2023 day 1 part 1

--- 2023Solutions/day01/part1.py
+++ 2023Solutions/day01/part1.py
@@ -1,12 +1,13 @@
 import os
 import sys
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
-data = [getints(x) for x in data]
+data = [re.findall(r"\d",x) for x in data]
 result = 0
 for line in data:
-    result += int(str(line[0])[0])*10 + int(str(line[-1])[-1])
+    result += int(line[0]) * 10 + int(line[-1])
 
 prco(result)
