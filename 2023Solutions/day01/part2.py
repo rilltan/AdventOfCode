9a17b852f8d6881a0124afc653273a48765304fb
rilltan <rohandwadhawan@gmail.com>
2023-12-01 23:28:27 +0000

    2023 day 1 part 2

--- 2023Solutions/day01/part2.py
+++ 2023Solutions/day01/part2.py
@@ -1,17 +1,20 @@
 import os
 import sys
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
 numbers = {"one":1,"two":2,"three":3,"four":4,"five":5,"six":6,"seven":7,"eight":8,"nine":9}
+numbersreversed = {x[::-1]:y for x,y in numbers.items()}
 for i in range(1,10):
     numbers[str(i)] = i
+    numbersreversed[str(i)] = i
 
 result = 0
 for line in data:
-    stuff = findalloverlap("|".join(numbers),line)
-    if stuff:
-        result += numbers[stuff[0]]*10 + numbers[stuff[-1]]
+    linereversed = line[::-1]
+    result += numbers[re.search("|".join(numbers),line).group()]*10
+    result += numbersreversed[re.search("|".join(numbersreversed),linereversed).group()]
 
 prco(result)
