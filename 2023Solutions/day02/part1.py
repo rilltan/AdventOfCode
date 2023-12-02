f0fc80d83ceed95ea4e1d61f4a7fb8ce8824dd37
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-02 05:16:12 +0000

    2023 day 2

--- 2023Solutions/day02/part1.py
+++ 2023Solutions/day02/part1.py
@@ -1,6 +1,22 @@
 import os
 import sys
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [x.split(" ")[2:] for x in data]
+result = 0
+for i,x in enumerate(data):
+    good = True
+    for j in range(len(x)//2):
+        if int(x[j*2]) > 12 and re.match("red",x[j*2+1]):
+            good = False
+        if int(x[j*2]) > 13 and re.match("green",x[j*2+1]):
+            good = False
+        if int(x[j*2]) > 14 and re.match("blue",x[j*2+1]):
+            good = False
+    if good:
+        result += i+1
+
+prco(result)
--- 2023Solutions/day02/part2.py
+++ 2023Solutions/day02/part2.py
@@ -1,6 +1,23 @@
 import os
 import sys
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [x.split(" ")[2:] for x in data]
+result = 0
+for i,x in enumerate(data):
+    red = 0
+    blue = 0
+    green = 0
+    for j in range(len(x)//2):
+        if int(x[j*2]) > red and re.match("red",x[j*2+1]):
+            red = int(x[j*2])
+        if int(x[j*2]) > green and re.match("green",x[j*2+1]):
+            green = int(x[j*2])
+        if int(x[j*2]) > blue and re.match("blue",x[j*2+1]):
+            blue = int(x[j*2])
+    result += red*green*blue
+
+prco(result)
