12ee172f336ca47febd976f1ccc46a664b00f910
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-03 06:02:55 +0000

    2023 day 3

--- 2023Solutions/day03/part1.py
+++ 2023Solutions/day03/part1.py
@@ -1,6 +1,33 @@
 import os
 import sys
+import re
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+grid = {}
+gridunique = {}
+for r in range(len(data)):
+    count = 0
+    prev = False
+    ints = [abs(x) for x in getints(data[r])]
+    for c in range(len(data[0])):
+        if re.match(r"\d",data[r][c]):
+            grid[(r,c)] = ints[count]
+            gridunique[(r,c)] = (r,count)
+            prev = True
+        elif prev:
+            count += 1
+            prev = False
+
+result = 0
+added = set()
+for r in range(len(data)):
+    for c in range(len(data[0])):
+        if re.match(r"[^\w.]",data[r][c]):
+            for ra,ca in u_adjacent:
+                if (r+ra,c+ca) in grid and gridunique[r+ra,c+ca] not in added:
+                    added.add(gridunique[r+ra,c+ca])
+                    result += grid[r+ra,c+ca]
+
+prco(result)
