55416cc0bc40ef860bd89b3540fef375485603a8
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-04 05:18:47 +0000

    2023 day 4

--- 2023Solutions/day04/part1.py
+++ 2023Solutions/day04/part1.py
@@ -4,3 +4,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [x.split()[2::] for x in data]
+data = [splitlist(x, "|") for x in data]
+
+result = 0
+for line in data:
+    matches = set(line[0]) & set(line[1])
+    if matches:
+        result += 2**(len(matches)-1)
+        
+prco(result)
--- 2023Solutions/day04/part2.py
+++ 2023Solutions/day04/part2.py
@@ -4,3 +4,13 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [x.split()[2::] for x in data]
+data = [splitlist(x, "|") for x in data]
+
+copies = [1]*len(data)
+for i,line in enumerate(data):
+    matches = set(line[0]) & set(line[1])
+    for j in range(len(matches)):
+        copies[i+j+1] += copies[i]
+
+prco(sum(copies))
