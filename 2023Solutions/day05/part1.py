17ce560914b3c0bb90eda316408bbd4253593241
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-05 07:15:06 +0000

    2023 day 5 part 1

--- 2023Solutions/day05/part1.py
+++ 2023Solutions/day05/part1.py
@@ -4,3 +4,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [getints(x) for x in data]
+seeds = data[0]
+
+seedmap = {}
+for i,x in enumerate(data[1::]):
+    if not x:
+        mapped = set()
+    else:
+        for j in range(len(seeds)):
+            if inbounds(seeds[j],x[1],x[1]+x[2]-1) and j not in mapped:
+                if j == 1:
+                    print(x)
+                mapped.add(j)
+                seeds[j] += -x[1]+x[0]
+
+        print(seeds)
+prco(min(seeds))
--- 2023Solutions/day05/part2.py
+++ 2023Solutions/day05/part2.py
@@ -4,3 +4,35 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+data = [getints(x) for x in data]
+seedranges = []
+for i in range(len(data[0])//2):
+    seedranges.append((data[0][i*2],data[0][i*2]+data[0][i*2+1]-1))
+seedmaps = [[]]
+j=0
+for x in data[1::]:
+    if x:
+        seedmaps[j].append(x)
+    else:
+        j+=1
+        seedmaps.append([])
+seedmaps = [x for x in seedmaps if x]
+seedmaps = [sorted(x,key=lambda y:y[1]) for x in seedmaps]
+
+print(seedmaps)
+print(seedranges)
+
+for maptype in seedmaps:
+    newranges = []
+    print(seedranges)
+    for srange in seedranges:
+        sources = [(x[1],x[1]+x[2]-1) for x in maptype]
+        #print(sources)
+        if srange[0] < sources[0][0]:
+            newranges.append((srange[0],sources[0][0]-1))
+        for smap in maptype:
+            if srange[0] <= smap[1]+smap[2]-1 and srange[1] >= smap[1]:
+                newranges.append((max(srange[0],smap[1])+smap[0]-smap[1],min(srange[1],smap[1]+smap[2]-1)+smap[0]-smap[1]))
+        if srange[1] > sources[-1][1]:
+            newranges.append((sources[-1][1]+1,srange[1]))
+    seedranges = [x for x in newranges]
