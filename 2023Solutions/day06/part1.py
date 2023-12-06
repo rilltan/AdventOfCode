f1d1ad1c53da5b180be687e46008a1ee98fbea83
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-06 05:34:18 +0000

    2023 day 6

--- 2023Solutions/day06/part1.py
+++ 2023Solutions/day06/part1.py
@@ -4,3 +4,15 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+times = getints(data[0])
+dists = getints(data[1])
+
+result = 1
+for i in range(len(times)):
+    ways = 0
+    for j in range(times[i]):
+        if j * (times[i]-j) > dists[i]:
+            ways += 1
+    result *= ways
+
+prco(result)
--- 2023Solutions/day06/part2.py
+++ 2023Solutions/day06/part2.py
@@ -4,3 +4,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+time = int("".join([str(x) for x in getints(data[0])]))
+dist = int("".join([str(x) for x in getints(data[1])]))
+
+ways = 0
+for j in range(time):
+    if j * (time-j) > dist:
+        ways += 1
+
+prco(ways)
