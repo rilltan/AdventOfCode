b438bb3a41c5097d2b219c26e66cf02e4aba98c8
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-09 05:24:01 +0000

    2023 day 9

--- 2023Solutions/day09/part1.py
+++ 2023Solutions/day09/part1.py
@@ -4,3 +4,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+def all_zeroes(x):
+    allzeroes = True
+    for i in x:
+        if i != 0:
+            allzeroes = False
+            break
+    return allzeroes
+
+seqs = [getints(x) for x in data]
+result = 0
+
+for seq in seqs:
+    difs = [[x for x in seq]]
+    while not all_zeroes(difs[-1]):
+        difs.append([])
+        for i in range(len(difs[-2])-1):
+            difs[-1].append(difs[-2][i+1]-difs[-2][i])
+    result += sum(x[-1] for x in difs)
+
+prco(result)
