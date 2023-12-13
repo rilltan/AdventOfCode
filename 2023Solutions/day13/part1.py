c41ac58108a519421038776d28d2952fdc25022b
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-13 05:38:33 +0000

    2023 day 13

--- 2023Solutions/day13/part1.py
+++ 2023Solutions/day13/part1.py
@@ -3,4 +3,31 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
+data = splitlist(data,"")
 
+result = 0
+for grid in data:
+    for c in range(1,len(grid[0])):
+        found = True
+        for r in grid:
+            half1 = r[:c][::-1]
+            half2 = r[c:]
+            length = min(len(half1),len(half2))
+            if half1[:length] != half2[:length]:
+                found = False
+        if found:
+            result += c
+            break
+    
+    if not found:
+        for r in range(1,len(grid)):
+            found = True
+            half1 = grid[:r][::-1]
+            half2 = grid[r:]
+            if any(row1 != row2 for row1,row2 in zip(half1,half2)):
+                found = False
+            if found:
+                result += 100*r
+                break
+
+prco(result)
--- 2023Solutions/day13/part2.py
+++ 2023Solutions/day13/part2.py
@@ -3,4 +3,34 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
+data = splitlist(data,"")
 
+result = 0
+for grid in data:
+    for c in range(1,len(grid[0])):
+        errors = 0
+        for r in grid:
+            half1 = r[:c][::-1]
+            half2 = r[c:]
+            for letter1,letter2 in zip(half1,half2):
+                if letter1 != letter2:
+                    errors += 1
+        if errors == 1:
+            result += c
+            break
+    
+    if errors != 1:
+        for r in range(1,len(grid)):
+            half1 = grid[:r][::-1]
+            half2 = grid[r:]
+            errors = 0
+            for row1,row2 in zip(half1,half2):
+                if row1!=row2:
+                    for letter1,letter2 in zip(row1,row2):
+                        if letter1!=letter2:
+                            errors += 1
+            if errors == 1:
+                result += 100*r
+                break
+
+prco(result)
