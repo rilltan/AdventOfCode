832cdd76e254c8545692563633ff900678412069
Rohan Wadhawan <120060738+rilltan@users.noreply.github.com>
2023-12-23 07:45:45 +0000

    2023 day 23

--- 2023Solutions/day23/part1.py
+++ 2023Solutions/day23/part1.py
@@ -4,3 +4,25 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+dirs = {"<":(0,-1),">":(0,1),"v":(1,0),"^":(-1,0)}
+
+sys.setrecursionlimit(3000)
+
+def dfs(r,c,seen:set,longest):
+    seen.add((r,c))
+    if r==len(data)-1:
+        seen.remove((r,c))
+        return len(seen)
+    if data[r][c] !="." and data[r][c]!="#":
+        ra = dirs[data[r][c]][0]
+        ca = dirs[data[r][c]][1]
+        if (ra+r,ca+c) not in seen:
+            longest = max(longest, dfs(r+ra,c+ca,seen,longest))
+    else:
+        for ra,ca in u_cardinals:
+            if data[ra+r][c+ca] != "#" and (ra+r,ca+c) not in seen:
+                longest = max(longest, dfs(r+ra,c+ca,seen,longest))
+    seen.remove((r,c))
+    return longest
+
+prco(dfs(0,1,set(),0))
--- 2023Solutions/day23/part2.py
+++ 2023Solutions/day23/part2.py
@@ -4,3 +4,64 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+dirs = {"<":(0,-1),">":(0,1),"v":(1,0),"^":(-1,0)}
+
+junctions = []
+junctions.append((0,1))
+for r in range(len(data)):
+    for c in range(len(data[0])):
+        if data[r][c]==".":
+            count = 0
+            for ra,ca in u_cardinals:
+                if not inbounds(r+ra,0,len(data)-1) or not inbounds(c+ca,0,len(data)-1):
+                    continue
+                if data[r+ra][c+ca] in dirs:
+                    count += 1
+            if count > 1:
+                junctions.append((r,c))
+junctions.append((len(data)-1,len(data[0])-2))
+
+graph = {i:[] for i in range(len(junctions)-1)}
+
+for i in range(len(junctions)-1):
+    for ra,ca in u_cardinals:
+        r = junctions[i][0]
+        c = junctions[i][1]
+        path = [(r,c),(r+ra,c+ca)]
+        found = False
+        while not found:
+            r = path[-1][0]
+            c = path[-1][1]
+            stepped = False
+            for rb,cb in u_cardinals:
+                if not inbounds(r+ra,0,len(data)-1) or not inbounds(c+ca,0,len(data)-1):
+                    continue
+                if data[r+rb][c+cb] != "#" and (r+rb,c+cb) != path[-2]:
+                    stepped = True
+                    path.append((r+rb,c+cb))
+                    break
+            if not stepped:
+                found = True
+            if (data[path[-2][0]][path[-2][1]] in dirs or path[-1][0]==len(data)-1) and path[-1] in junctions:
+                graph[i].append((junctions.index(path[-1]),len(path)-1))
+                found = True
+
+seen = set()
+def dfs(index,path,longest):
+    global seen
+    
+    if index == len(junctions) - 1:
+        if path > longest:
+            print(path)
+        return path
+
+    for node in graph[index]:
+        if node[0] not in seen:
+            seen.add(node[0])
+            longest = max(longest,dfs(node[0],path+node[1],longest))
+            seen.remove(node[0])
+    return longest
+
+prco(dfs(0,0,0))
+
+# Currently takes about 1 minute to run, will improve later
