280e6b28351c9553f843864529f35134ca686855
rilltan <rohandwadhawan@gmail.com>
2024-06-08 14:26:47 +0100

    2015 day 7

--- 2015Solutions/day07/part2.py
+++ 2015Solutions/day07/part2.py
@@ -4,3 +4,34 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+asignal = int(input("Enter signal for a from part 1: "))
+
+wires = {}
+for line in data:
+    gate,output = line.split(" -> ")
+    gate = gate.split()
+    wires[output] = gate
+wires["b"] = asignal
+
+def signal(x):
+    if x.isdigit():
+        return int(x)
+    if type(wires[x]) is int:
+        return wires[x]
+     
+    if len(wires[x]) == 1:
+        wires[x] = signal(wires[x][0])
+    elif len(wires[x]) == 2:
+        wires[x] = ~signal(wires[x][1]) & 65535
+    elif wires[x][1] == "OR":
+        wires[x] = signal(wires[x][0]) | signal(wires[x][2])
+    elif wires[x][1] == "AND":
+        wires[x] = signal(wires[x][0]) & signal(wires[x][2])
+    elif wires[x][1] == "LSHIFT":
+        wires[x] = signal(wires[x][0]) << int(wires[x][2])
+    elif wires[x][1] == "RSHIFT":
+        wires[x] = signal(wires[x][0]) >> int(wires[x][2])
+    
+    return wires[x]
+
+prco(signal("a"))
