660c435582742efff7d30768514448f0d21a2068
rohan <rohandwadhawan@gmail.com>
2024-12-02 14:10:30 +0000

    CuCATS2024 day 2

--- CuCATS2024Solutions/day02/solution.py
+++ CuCATS2024Solutions/day02/solution.py
@@ -2,5 +2,20 @@ import os
 import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
-data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
+data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))[0]
 
+out = ""
+num = 0
+i=25
+for char in data:
+    if char.isalpha():
+        out += chr((ord(char)-97+i)%26 + 97)
+    else:
+        out += char
+    i-=1
+    num += 1
+    if num%16 == 0:
+        i-=10
+
+
+print(out)
