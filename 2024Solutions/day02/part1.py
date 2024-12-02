be12c71e139e6464435476562dd08652f227bfc8
rohan <rohandwadhawan@gmail.com>
2024-12-02 05:29:09 +0000

    2024 day 2

--- 2024Solutions/day02/part1.py
+++ 2024Solutions/day02/part1.py
@@ -4,3 +4,21 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+def safe(ns):
+    good = True
+    prev = ns[0]-ns[1] < 0
+    for i in range(len(ns)-1):
+        if abs(ns[i]-ns[i+1])>3:
+            good = False
+        if abs(ns[i]-ns[i+1])<1:
+            good = False
+        decrease = ns[i]-ns[i+1] < 0
+        if decrease != prev:
+            good = False
+    return good
+
+result = 0
+for line in data:
+    result += safe(getints(line))
+
+prco(result)
--- 2024Solutions/day02/part2.py
+++ 2024Solutions/day02/part2.py
@@ -4,3 +4,27 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+def safe(ns):
+    good = True
+    prev = ns[0]-ns[1] < 0
+    for i in range(len(ns)-1):
+        if abs(ns[i]-ns[i+1])>3:
+            good = False
+        if abs(ns[i]-ns[i+1])<1:
+            good = False
+        decrease = ns[i]-ns[i+1] < 0
+        if decrease != prev:
+            good = False
+    return good
+
+result = 0
+for line in data:
+    ns = getints(line)
+    for j in range(len(ns)+1):
+        good = True
+        newns = ns[:j] + ns[j+1:]
+        if safe(newns):
+            result+=1
+            break
+
+prco(result)
