e4609204c8b1cf1c2a51d0dd7db2b18a3f92c207
rohan <rohandwadhawan@gmail.com>
2024-12-04 05:15:47 +0000

    2024 day 4

--- 2024Solutions/day04/part1.py
+++ 2024Solutions/day04/part1.py
@@ -4,3 +4,23 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+x=set()
+m=set()
+a=set()
+s=set()
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if data[i][j] == "X":
+            x.add((i,j))
+        if data[i][j] == "M":
+            m.add((i,j))
+        if data[i][j] == "A":
+            a.add((i,j))
+        if data[i][j] == "S":
+            s.add((i,j))
+r=0
+for loc in x:
+    for dir in u_adjacent:
+        if (loc[0]+dir[0],loc[1]+dir[1]) in m and (loc[0]+2*dir[0],loc[1]+2*dir[1]) in a and (loc[0]+3*dir[0],loc[1]+3*dir[1]) in s:
+            r+=1
+prco(r)
--- 2024Solutions/day04/part2.py
+++ 2024Solutions/day04/part2.py
@@ -4,3 +4,30 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+x=set()
+m=set()
+a=set()
+s=set()
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if data[i][j] == "X":
+            x.add((i,j))
+        if data[i][j] == "M":
+            m.add((i,j))
+        if data[i][j] == "A":
+            a.add((i,j))
+        if data[i][j] == "S":
+            s.add((i,j))
+r=0
+for i in range(len(data)):
+    for j in range(len(data[0])):
+        if (i,j) in m and (i,j+2) in m and (i+2,j) in s and (i+2,j+2) in s and (i+1,j+1) in a:
+            r += 1
+        if (i,j) in m and (i,j+2) in s and (i+2,j) in m and (i+2,j+2) in s and (i+1,j+1) in a:
+            r += 1
+        if (i,j) in s and (i,j+2) in s and (i+2,j) in m and (i+2,j+2) in m and (i+1,j+1) in a:
+            r += 1
+        if (i,j) in s and (i,j+2) in m and (i+2,j) in s and (i+2,j+2) in m and (i+1,j+1) in a:
+            r += 1
+        
+prco(r)
