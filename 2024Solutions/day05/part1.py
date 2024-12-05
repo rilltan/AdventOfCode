23c1b90622161b773dd31b5f032b39966091cc6d
rohan <rohandwadhawan@gmail.com>
2024-12-05 05:31:17 +0000

    2024 day 5

--- 2024Solutions/day05/part1.py
+++ 2024Solutions/day05/part1.py
@@ -4,3 +4,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+rules = set()
+i=0
+while data[i] != "":
+    nums = getints(data[i])
+    rules.add((nums[0],nums[1]))
+    i+=1
+
+r = 0
+i+=1
+while i < len(data):
+    nums = getints(data[i])
+    i+=1
+    if any((nums[j+1],nums[j]) in rules for j in range(len(nums)-1)):
+        continue
+    r += nums[len(nums)//2]
+
+prco(r)
--- 2024Solutions/day05/part2.py
+++ 2024Solutions/day05/part2.py
@@ -3,4 +3,29 @@ import sys
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
+from functools import cmp_to_key
 
+rules = set()
+i=0
+while data[i] != "":
+    nums = getints(data[i])
+    rules.add((nums[0],nums[1]))
+    i+=1
+
+def compare_nums(n1,n2):
+    if (n1,n2) in rules:
+        return -1
+    else:
+        return 1
+
+i+=1
+r = 0
+while i < len(data):
+    nums = getints(data[i])
+    i+=1
+    if not any((nums[j+1],nums[j]) in rules for j in range(len(nums)-1)):
+        continue
+    nums.sort(key=cmp_to_key(compare_nums))
+    r += nums[len(nums)//2]
+
+prco(r)
