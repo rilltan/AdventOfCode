fb2d58d458910dd56a06910ca0b0abe82222b94e
rohan <rohandwadhawan@gmail.com>
2024-12-14 05:51:27 +0000

    2024 day 14

--- 2024Solutions/day14/part1.py
+++ 2024Solutions/day14/part1.py
@@ -4,3 +4,28 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+ps = []
+vs = []
+for line in data:
+    nums = getints(line)
+    ps.append((nums[0],nums[1]))
+    vs.append((nums[2],nums[3]))
+
+for j in range(100):
+    for i in range(len(ps)):
+        ps[i] = ((ps[i][0]+vs[i][0])%101,(ps[i][1]+vs[i][1])%103)
+
+q1=0
+q2=0
+q3=0
+q4=0
+for i in ps:
+    if i[0]<50 and i[1]<51:
+        q1+=1
+    elif i[0]>50 and i[1]<51:
+        q2+=1
+    elif i[0]<50 and i[1]>51:
+        q3+=1
+    elif i[0]>50 and i[1]>51:
+        q4+=1
+prco(q1*q2*q3*q4)
--- 2024Solutions/day14/part2.py
+++ 2024Solutions/day14/part2.py
@@ -4,3 +4,29 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+ps = []
+vs = []
+for line in data:
+    nums = getints(line)
+    ps.append((nums[0],nums[1]))
+    vs.append((nums[2],nums[3]))
+
+flag = True
+secs = 0
+while flag:
+    secs += 1
+    b = set()
+    for i in range(len(ps)):
+        ps[i] = ((ps[i][0]+vs[i][0])%101,(ps[i][1]+vs[i][1])%103)
+        b.add(ps[i])
+    if len(b) == len(ps):
+        flag = False
+
+for y in range(103):
+    for x in range(101):
+        if (x,y) in b:
+            print("#",end="")
+        else:
+            print(" ",end="")
+    print()
+prco(secs)
