b906d0b1b39d101e0f2ddf7eab122118b9f890e1
rohan <rohandwadhawan@gmail.com>
2024-12-20 13:46:54 +0000

    2024 CuCATS day 20

--- CuCATS2024Solutions/day20/solution.py
+++ CuCATS2024Solutions/day20/solution.py
@@ -4,3 +4,21 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+import functools
+@functools.lru_cache(None)
+def score(s,clipboardUsed):
+    if s == "":
+        return 0
+    best = 1 + score(s[:-1],clipboardUsed)
+    for i in range(1,len(s)//2+1):
+        end = s[-i:]
+        if end in s[:-i]:
+            best = min(best, (1 if clipboardUsed==s[-i:] else 2) + score(s[:-i],s[-i:]))
+    return best
+
+r = 0
+for i,x in enumerate(data[1:]):
+    r += score(x,"")
+    if (i+1)%10 == 0:
+        print(f"{i+1}% of lines completed")
+prco(r)
