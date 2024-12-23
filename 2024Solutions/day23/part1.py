3772dea49c34b0f8aae8d4e445643945ec14e749
rohan <rohandwadhawan@gmail.com>
2024-12-23 05:45:49 +0000

    2024 day 23

--- 2024Solutions/day23/part1.py
+++ 2024Solutions/day23/part1.py
@@ -4,3 +4,20 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+graph = {}
+for line in data:
+    x,y = line.split("-")
+    if x not in graph:
+        graph[x] = []
+    if y not in graph:
+        graph[y] = []
+    graph[x].append(y)
+    graph[y].append(x)
+
+r = 0
+for a,b,c in it.combinations(graph.keys(),3):
+    if "t" == a[0] or "t" == b[0] or "t" == c[0]:
+        if b in graph[a] and c in graph[a] and b in graph[c]:
+            r += 1
+
+prco(r)
--- 2024Solutions/day23/part2.py
+++ 2024Solutions/day23/part2.py
@@ -4,3 +4,24 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
+graph = {}
+for line in data:
+    x,y = line.split("-")
+    if x not in graph:
+        graph[x] = []
+    if y not in graph:
+        graph[y] = []
+    graph[x].append(y)
+    graph[y].append(x)
+
+groups = set([tuple([x]) for x in graph])
+while len(groups)>1:
+    newgroups = set()
+    for g in groups:
+        for d in graph[g[0]]:
+            if d not in g:
+                if all(d in graph[a] for a in g):
+                    newgroups.add(tuple(sorted(list(g) + [d])))
+    groups = newgroups
+
+prco(",".join(groups.pop()))
