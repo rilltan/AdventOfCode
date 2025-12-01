e50404df487d92f65bc1d6ad7c39a7b410e73161
rohan <rohandwadhawan@gmail.com>
2025-12-01 05:43:19 +0000

    2025 day1 part2

--- 2025Solutions/day01/part2.py
+++ 2025Solutions/day01/part2.py
@@ -4,18 +4,12 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
 from utilities import *
 data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
 
-import os
-import sys
-sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-from utilities import *
-data = loadinput(os.path.join(os.path.dirname(__file__),"input.txt"))
-
 x = 50
 r = 0
 for line in data:
     a = x == 0
     x += int(line[1:]) * (-1 if line[0]=="L" else 1)
-    
+
     if x >= 100:
         r += abs(x)//100
     elif x <= 0:
